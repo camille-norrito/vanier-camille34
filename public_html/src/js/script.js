6125894bc98fb108eb6d777a84e831a3fae31729
camille <camille.norrito@edu.univ-fcomte.fr>
2025-09-09 13:29:02 -0400

    firsts

---
+++ public_html/src/js/script.js
@@ -0,0 +1,16 @@
+// On cible les éléments à modifier
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+const page = document.body;
+
+// Vérifier si les éléments existent avant d'ajouter l'événement
+if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+        const isOpen = toggle.ariaExpanded === "true";
+        const isClosed = !isOpen;
+        // Mise à jour des attributs ARIA pour accessibilité
+        toggle.ariaExpanded = isClosed;
+        nav.ariaHidden = isOpen;
+        page.classList.toggle("noscroll", isClosed);
+    });
+}
